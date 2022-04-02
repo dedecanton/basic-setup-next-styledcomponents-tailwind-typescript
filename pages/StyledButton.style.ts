32874d0ce04ef2fd026071c60ed543b55e77e6a5
dedecanton <andrecanton18@gmail.com>
2022-04-01 22:22:17 -0300

    setup

---
+++ pages/StyledButton.style.ts
@@ -0,0 +1,14 @@
+import tw from 'twin.macro'
+
+export const TailwindButton = tw.button`
+  bg-red-500
+  hover:bg-blue-500
+  text-white
+  font-bold
+  py-2
+  px-4
+  border
+  border-black
+  rounded
+  text-2xl
+`
